770a4e330b3d0aed6375d0722ccfa1c547664dc8
Shahzad Qadir <shahzadqadir@hotmail.co.uk>
2025-11-10 13:54:11 +0000

    Update Admin and added Forms for Host Creation

--- ztp/admin.py
+++ ztp/admin.py
@@ -1,3 +1,12 @@
+# ~/automation/fullstack_ztp/ztp/admin.py
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import BGP, BGPNeighbor, BGPNetwork, Interface, StaticRoute, Host
+
+admin.site.register(BGP)
+admin.site.register(BGPNeighbor)
+admin.site.register(BGPNetwork)
+admin.site.register(Interface)
+admin.site.register(StaticRoute)
+admin.site.register(Host)
