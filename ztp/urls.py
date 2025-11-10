e32aba0c08b004c196ac0a5fa77cce9e0156f37b
Shahzad Qadir <shahzadqadir@hotmail.co.uk>
2025-11-10 13:34:58 +0000

    Completed DHCP Server CRUD Operations - Chapter 15

--- ztp/urls.py
+++ ztp/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('create_scp_server/', views.create_scp_server_view, name='create_scp_server'),
     path('list_scp_servers/', views.list_scp_servers_view, name='list_scp_servers'),
     path('edit_scp_server/<int:id>/', views.edit_scp_server_view, name='edit_scp_server'),
-    path('delete_scp_server/<int:id>/', views.delete_scp_server_view, name='delete_scp_server'),    # NEW
+    path('delete_scp_server/<int:id>/', views.delete_scp_server_view, name='delete_scp_server'),
+    path('create_dhcp_server/', views.create_dhcp_server_view, name='create_dhcp_server'),
+    path('list_dhcp_servers/', views.list_dhcp_servers_view, name='list_dhcp_servers'),
+    path('edit_dhcp_server/<int:id>/', views.edit_dhcp_server_view, name='edit_dhcp_server'),
+    path('delete_dhcp_server/<int:id>/', views.delete_dhcp_server_view, name='delete_dhcp_server'), # NEW
 ]
--- ztp/views.py
+++ ztp/views.py
@@ -1,11 +1,15 @@
-# ~/automation/fullstack_ztp/ztp/views.py
+# automation/fullstack_ztp/ztp/views.py
 
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.urls import reverse
 
-from .forms import SCPServerForm
 from . import models
+
+# Create your views here.
+
+from .forms import SCPServerForm, DHCPServerForm    # New import DHCPServerForm
+
 
 def create_scp_server_view(request):
     form = SCPServerForm()
@@ -16,13 +20,11 @@ def create_scp_server_view(request):
             return redirect(reverse('homepage'))
     return render(request, 'ztp/create_scp_server.html', {'form': form})
 
-
 def list_scp_servers_view(request):
     scp_servers = models.SCPServer.objects.all()
     return render(request, 'ztp/list_scp_servers.html', {'scp_servers': scp_servers})
 
-
-def edit_scp_server_view(request, id):  # NEW
+def edit_scp_server_view(request, id):
     obj = models.SCPServer.objects.get(id=id)
     form = SCPServerForm(instance=obj)
     if request.method == "POST":
@@ -37,3 +39,34 @@ def delete_scp_server_view(request, id):
     obj = models.SCPServer.objects.get(id=id)
     obj.delete()
     return redirect(reverse('list_scp_servers'))
+
+
+def create_dhcp_server_view(request):
+    form = DHCPServerForm()
+    if request.method == "POST":
+        form = DHCPServerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('homepage'))
+    return render(request, 'ztp/create_dhcp_server.html', {'form': form})
+
+def list_dhcp_servers_view(request):
+    dhcp_servers = models.DHCPServer.objects.all()
+    return render(request, 'ztp/list_dhcp_servers.html', {'dhcp_servers': dhcp_servers})
+
+
+def edit_dhcp_server_view(request, id):
+    obj = models.DHCPServer.objects.get(id=id)
+    form = DHCPServerForm(instance=obj)
+    if request.method == "POST":
+        form = DHCPServerForm(request.POST, instance=obj)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('list_dhcp_servers'))
+    return render(request, 'ztp/edit_dhcp_server.html', {'form': form})
+
+
+def delete_dhcp_server_view(request, id):
+    obj = models.DHCPServer.objects.get(id=id)
+    obj.delete()
+    return redirect(reverse('list_dhcp_servers'))
